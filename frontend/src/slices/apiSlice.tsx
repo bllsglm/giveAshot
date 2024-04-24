593a9b1b33c9c689f737d595ff3a4de254533a4c
bllsglm <bilalsaglam92@gmail.com>
2024-04-24 17:52:44 +0300

    giveaShot url

--- frontend/src/slices/apiSlice.tsx
+++ frontend/src/slices/apiSlice.tsx
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const BASE_URL =
   import.meta.env.VITE_NODE_ENV === 'development'
     ? 'http://localhost:5000/api'
-    : 'abc'
+    : 'https://giveashot.onrender.com/'
 
 const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL, credentials: 'include' })
 
